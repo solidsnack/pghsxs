393c48fe0a42a498463a8e85d69e6f81ac5a37fc
Jason Dusek <jason.dusek@gmail.com>
2010-07-29 13:01:03 -0700

    Not sure what is up with my parser.

--- Database/PGHSXS/PGTypes/Parser.hs
+++ Database/PGHSXS/PGTypes/Parser.hs
@@ -5,7 +5,7 @@
 module Database.PGHSXS.PGTypes.Parser where
 
 import Prelude hiding (takeWhile)
-import Control.Applicative ((<$>), (<*>), pure, some)
+import Control.Applicative ((<$>), (<*>), (<$), (<|>), pure, some)
 import Data.ByteString (append, ByteString)
 
 import Text.CSV.ByteString
@@ -14,7 +14,7 @@ import Data.Attoparsec.Char8
 
 parse_table_info             =  manyTill parse_table_info_line empty_ending
  where
-  empty_ending               =  many endOfLine >> endOfInput
+  empty_ending               =  endOfLine
 
 parse_table_info_line        =  do
   some (char ' ')
@@ -22,13 +22,27 @@ parse_table_info_line        =  do
   separator
   typ_string                <-  manyTill anyChar separator
   not_null                  <-  option False (string "not null" >> pure True)
-  endOfLine
   pure (name, typ_string, not_null)
+
+typ_column_name =
+  append <$> string "typ" <*> takeWhile (isDigit .|| isAlpha_ascii)
+
+working = do
+  char ' '
+  name <- typ_column_name
+  separator
+  typ_string                <-  manyTill anyChar separator
+  not_null <- takeWhile (/= '\n')
+--not_null                  <-  ("not null\n" ?>> True) <|> ("\n" ?>> False)
+--  The top one leads to a terminating parser. The bottom one does not.
+  endOfLine
+  return (name, typ_string, not_null)
  where
-  typ_column_name            =  do
-    string "typ"
-    append "typ" <$> takeWhile (isDigit .|| isAlpha_ascii)
-  separator                  =  some (char ' ') >> char '|' >> some (char ' ')
+  bytes ?>> val = val <$ string bytes
+
+
+
+separator                  =  some (char ' ') >> char '|' >> some (char ' ')
     
 
 
